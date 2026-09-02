52bb2b4b6e24ab65566b6b1f298362cbf8624607
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-02 22:23:15 +0800

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,18 @@
+package latihan; // Deklarasi package untuk mengelompokan dalam satu folder
+
+import java.util.Scanner; // Impor Library/data luar untuk digunakan
+
+public class day1 { // Class menjadi tempat utama untuk menulis code
+    public static void main(String[] args) { // Main Method adalah tempat program di eksekusi
+        // komen satu baris
+      
+        /*
+         * komen multi baris
+         */
+  
+         /**
+          * komen dokumentasi biasa digunakan untuk memberi penjelasan detail biasanya pada method
+          * @author Nama Anda(seperti ini)
+          */
+    }
+}
