99a215603e6f3a34631a13b23d328158a7443319
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-03 14:45:54 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,16 @@
+public class Day2 {
+    public static void main(String[] args) {
+
+        // Menggunakan print() untuk mencetak teks tanpa berpindah kebaris baru
+        System.out.print("=== ");
+        System.out.print(" BIODATA ");
+        System.out.print("===\n");
+
+        // Menggunakan println() untuk mencetak teks yg akan otomatis ke baris baru
+        System.out.println("Nama Lengkap  : Ahmad Muchlis");
+        System.out.println("Alamat        : Baru, Kec.Luyo");
+
+        // Menggunakan printf() untuk mencetak teks dengan format tertentu
+        System.out.printf("Tinggi %f cm dan berat %d kg", 172.5, 65);
+    }
+}
