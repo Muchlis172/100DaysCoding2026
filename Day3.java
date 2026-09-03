056e71ade33168c73c200ef4266740f6f65b807d
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-04 01:30:32 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,16 @@
+public class Day3 {
+    public static void main(String[] args) {
+        
+        // \n untuk membuat baris baru
+        System.out.println("Teks Baris 1\nTeks Baris 2");
+
+        // \t untuk emberi jarak tab/spasi lebar
+        System.out.println("Item:\tflutter");
+
+        // \" untuk Menampilkan tanda kutip
+        System.out.println("Dia berkata, \"Ngoding yuk\"");
+
+        // \\ untuk Menampilkan satu garis miring 
+        System.out.println("Folder: C:\\Andorid\\SDK");
+    }
+}
