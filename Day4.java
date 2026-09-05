e3f996cd32cf822dea44ca25e15ced279aaf3df9
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-05 21:30:34 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,14 @@
+public class Day4 {
+    public static void main(String[]args) {
+        
+        byte nilaiByte = Byte.MAX_VALUE;
+        short nilaiShort = Short.MAX_VALUE;
+        int nilaiInt = Integer.MAX_VALUE;
+        long nilaiLong = Long.MAX_VALUE;
+
+        System.out.println("panjang byte = " + nilaiByte);
+        System.out.println("panjang short = " + nilaiShort);
+        System.out.println("panjang int = " + nilaiInt);
+        System.out.println("panjang long = " + nilaiLong);
+    }
+}
