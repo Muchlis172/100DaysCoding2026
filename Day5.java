572ead40f7bff3f4ae253bf4ef4c48ec856530d9
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-06 23:29:19 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,10 @@
+public class Day5 {
+    public static void main(String[]args) {
+        
+        double nilaiDouble = Double.MAX_VALUE;
+        float nilaiFloat = Float.MAX_VALUE;
+          
+        System.out.println("panjang Double = " + nilaiDouble);
+        System.out.println("panjang Float = " + nilaiFloat);
+    }
+}
