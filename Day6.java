0f9eb0d0e4920816924fd8094d509b3ed42679a5
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-07 22:15:58 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class Day6 {
+    public static void main(String[] args) {
+      
+        char nC = 'A';
+        boolean nB = true;
+
+        System.out.println("Nilai char = " + nC);
+        System.out.println("Nilai boolean = " + nB);
+    }
+}
