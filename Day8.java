44c79118102d312fe1f25feb4aba46d2f8e4c6d3
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-09 11:26:08 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,12 @@
+class Day8{
+  public static void main(String[]args){
+    //deklarasi variable
+    String nama;
+    //inisialisasi variable
+    nama = "Ahmad Muchlis";
+    System.out.print("nama = "+nama);
+    //update variable
+    nama = "Ahmad Muchlis bin Update";
+    System.out.print("nama = "+nama);
+  }
+}
