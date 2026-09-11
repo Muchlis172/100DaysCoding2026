92187170f51783956069bf584cbfae7b7797616a
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-11 21:14:41 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,35 @@
+public class Day10 {
+    public static void main(String[]args) {
+
+        byte minByte = Byte.MIN_VALUE;
+        byte maxByte = Byte.MAX_VALUE;
+        System.out.println("Nilai Minimal tipe data byte = "+minByte);
+        System.out.println("Nilai Maximal tipe data byte = "+maxByte);
+
+        short minShort = Short.MIN_VALUE;
+        short maxShort = Short.MAX_VALUE;
+        System.out.println("\nNilai Minimal tipe data short = "+minShort);
+        System.out.println("Nilai Maximal tipe data short = "+maxShort);
+
+        int minInt = Integer.MIN_VALUE;
+        int maxInt = Integer.MAX_VALUE;
+        System.out.println("\nNilai Minimal tipe data int = "+minInt);
+        System.out.println("Nilai Maximal tipe data int = "+maxInt);
+
+        long minLong = Long.MIN_VALUE;
+        long maxLong = Long.MAX_VALUE;
+        System.out.println("\nNilai Minimal tipe data long = "+minLong);
+        System.out.println("Nilai Maximal tipe data long = "+maxLong);
+
+        double minDouble = Double.MIN_VALUE;
+        double maxDouble = Double.MAX_VALUE;
+        System.out.println("\nNilai Minimal tipe data double = "+minDouble);
+        System.out.println("Nilai Maximal tipe data double = "+maxDouble);
+
+        float minFloat = Float.MIN_VALUE;
+        float maxFloat = Float.MAX_VALUE;
+        System.out.println("\nNilai Minimal tipe data float = "+minFloat);
+        System.out.println("Nilai Maximal tipe data float = "+maxFloat);
+
+    }
+}
