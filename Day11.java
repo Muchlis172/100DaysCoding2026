eb341c59a742b9e679b34abe5a76a5260d839659
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-12 00:06:13 +0800

    Update Day11.java

--- Day11.java
+++ Day11.java
@@ -4,7 +4,7 @@ class Day11{
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
     System.out.print("Masukkan nama = ");
-    String nama = sc.nextLine;
+    String nama = sc.nextLine();
     System.out.println("\nNama = "+nama);
   }
 }
