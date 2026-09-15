f046f73c4fe3bffd8435506d920660b2fab13434
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-15 21:28:44 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,8 @@
+public class Day14 {
+    public static void main(String[] args) {
+        int a = 15, b = 7;
+        
+        System.out.println("Penjumlahan : " + (a + b));
+        System.out.println("Pengurangan : " + (a - b));
+    }
+}
