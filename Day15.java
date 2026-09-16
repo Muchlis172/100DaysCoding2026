3c778dc608b609d224fcce0ff2ca342dff1bff64
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-16 21:40:02 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,8 @@
+public class Day15 {
+    public static void main(String[] args) {
+        double a = 15, b = 7;
+        
+        System.out.println("Penjumlahan : " + (a * b));
+        System.out.println("Pengurangan : " + (a / b));
+    }
+}
