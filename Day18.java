201c12c67fbeef10bdc55d93e29ff1943e781db0
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-19 20:37:09 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,13 @@
+public class Day18 {
+    public static void main(String[] args) {
+        
+        int nilaiInt = 100;
+      
+        long nilaiLong = nilaiInt;   
+        double nilaiDouble = nilaiInt; 
+        
+        System.out.println("Nilai Integer: " + nilaiInt);
+        System.out.println("Nilai Long: " + nilaiLong);
+        System.out.println("Nilai Double: " + nilaiDouble);
+    }
+}
