aa575d066e8f9e82bbecbf5b7d0b5ce08ae0e1f7
Muchlis172 <nurhidaya9631@gmail.com>
2026-09-26 22:53:48 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,8 @@
+public class LuasLingkaran {
+    public static void main(String[] args) {
+        double r = 7.0; 
+        double luas = 3.14 * r * r;
+
+        System.out.printf("Luas lingkaran dengan jari-jari %.2f adalah: %.2f\n", r, luas);
+    }
+}
